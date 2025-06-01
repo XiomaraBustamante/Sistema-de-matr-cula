cae7f23b1cecb5411547b3f36ef9c368c3f62bec
Xiomara Bustamante <xiomaraaabust@gmail.com>
2025-06-01 11:52:36 -0500

    hola

--- src/clases/Alumno.java
+++ src/clases/Alumno.java
@@ -1,9 +1,11 @@
 package clases;
 
 public class Alumno {
+	private int codAlumno;
 	private String nombres;
 	private String apellidos;
 	private String Dni;
 	private int edad;
+	private int estado;
 
 }
